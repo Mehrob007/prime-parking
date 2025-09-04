56408d94e629ba02c4e696eb2a56fe0a6ccabded
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-09-04 09:50:05 +0500

    add:  new componens for open parking

--- src/components/elements/BoxMid1.jsx
+++ src/components/elements/BoxMid1.jsx
@@ -6,12 +6,17 @@ import TextComponent from "./com/TextComponent";
 import { getData } from "../../function/getData";
 import P from "./com/P";
 import apiClient from "../../utils/apiClient";
+import AccordionCom from "./componentSite/AccordionCom";
 const keys = [
   "box_mid_1_content_1",
   "box_mid_1_content_2",
   "box_mid_1_content_3",
   "box_mid_1_content_4",
   "schema_parking",
+  "box_mid_1_title_list_5",
+  "box_mid_1_content_list_6",
+  "box_mid_1_title_list_7",
+  "box_mid_1_content_list_8",
 ];
 export default function BoxMid1() {
   const [data, setData] = useState();
@@ -81,6 +86,39 @@ export default function BoxMid1() {
             <span>Ознакомиться со схемой</span>
           </button>
         </nav>
+      </div>
+      <div className="content-bottom-box4">
+        <AccordionCom
+          title={data?.[5]}
+          activeStyle={{
+            height: "auto",
+          }}
+          htmlEl={
+            <ul className="ul-box4">
+              {data?.[6]?.split("$").map((e, i) => (
+                <li key={i}>
+                  <TextComponent>{e}</TextComponent>
+                </li>
+              ))}
+            </ul>
+          }
+        />
+        <AccordionCom
+          title={data?.[7]}
+          activeStyle={{
+            height: "auto",
+          }}
+          htmlEl={
+            <ul className="ul-box4">
+              {data?.[8]?.split("$").map((e, i) => (
+                <li key={i}>
+                  <TextComponent>{e}</TextComponent>
+                </li>
+              ))}
+            </ul>
+          }
+        />
+        <span className="line-545454"></span>
       </div>
     </div>
   );
