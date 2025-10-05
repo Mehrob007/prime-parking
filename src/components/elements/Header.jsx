1391fb16d61228d6c7ad16737f8c3101bd5b50f4
Mehrob <mehrobsanginov2006@gmail.com>
2025-10-05 14:27:49 +0500

    edit: nav-bar for burger menu site

--- src/components/elements/Header.jsx
+++ src/components/elements/Header.jsx
@@ -32,7 +32,10 @@ export default function Header() {
   return (
     <header>
       <div className="nav-bar">
-        {isMobile ? (
+        <main>
+          <img onClick={() => navigate("./")} src={PrimeParkingHeader} alt="" />
+        </main>
+        {/* {isMobile ? (
           <nav onClick={() => setModalOpen(modalOpen)}>
             <img src={PrimeParking} alt=" " />
           </nav>
@@ -41,9 +44,12 @@ export default function Header() {
             <NavLink to="./services">Услуги</NavLink>
             <NavLink to="./offer">Оферта</NavLink>
             <NavLink to="./parking-rules">Правила паркинга</NavLink>
+            <NavLink to="./parking-zoning">Зонирование</NavLink>
           </div>
-        )}
-        <img onClick={() => navigate("./")} src={PrimeParkingHeader} alt="" />
+        )} */}
+        <nav onClick={() => setModalOpen(modalOpen)}>
+          <img src={PrimeParking} alt=" " />
+        </nav>
         <a href="tel: +7 (495) 481 22 44">
           {!isMobile ? (
             <>
