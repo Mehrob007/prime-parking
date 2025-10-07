524b3d30b76403df1b74b62b4eaaade84003ffdb
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-10-07 11:39:59 +0500

    add: cash for data in the box1

--- src/components/elements/Box1.jsx
+++ src/components/elements/Box1.jsx
@@ -1,31 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import carBox1 from "../../assets/icon/car-box1.png";
 import awDown from "../../assets/icon/aw-down.svg";
 import AOS from "aos";
 import { getData } from "../../function/getData";
 import P from "./com/P";
+import { useSore } from "../../store/globalState";
 
 const keys = ["box1_content_1", "box1_content_2"];
 
 export default function Box1() {
-  const [data, setData] = useState([]);
+  const { dataMain, setData } = useSore();
+  const data = dataMain?.box1;
 
   const getItems = async () => {
     const res = await getData(keys);
-    console.log("res", res);
 
-    setData(res);
+    setData({ box1: res });
   };
 
   useEffect(() => {
-    getItems();
+    if (!data) getItems();
   }, []);
   useEffect(() => {
     AOS.init({
       duration: 1000,
     });
   }, []);
-  console.table("box1-main", data);
 
   return (
     <div className="box1-main">
