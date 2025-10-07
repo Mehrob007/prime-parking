102a5513cebd8b6717a470a77a5e3c4fbd5878cb
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-10-07 11:42:04 +0500

    create:  new  global state for data cash all projact

--- src/store/globalState.js
+++ src/store/globalState.js
@@ -1,9 +1,19 @@
 import { create } from "zustand";
 
-export const functions = create((set, get) => ({
+export const functions = create((set) => ({
   redirect: (url) => {
     window.open(url, "_blank");
   },
   modalOpen: false,
   setModalOpen: (state) => set({ modalOpen: !state }),
+}));
+
+export const useSore = create((set) => ({
+  dataMain: { def: "" },
+  setData: (state) => {
+    console.log("setData input:", state);
+    set((prev) => ({
+      dataMain: {...prev.dataMain, ...state},
+    }));
+  },
 }));
