dc472570b1a15a1afc6dc9defebc97ca51147efc
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-10-07 11:38:47 +0500

    edit: if scroll to top

--- src/app/router/Router.jsx
+++ src/app/router/Router.jsx
@@ -2,9 +2,9 @@ import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "../../components/elements/Header.jsx";
 import ScrollToTop from "../../components/elements/com/ScrollToTop.jsx";
-import { functions } from "../../store/globalState.js";
-import ModalMenu from "../../components/elements/componentSite/ModalMenu.jsx";
 import FileViewer from "../../components/elements/componentSite/FileViewer.jsx";
+import { useLocation } from "react-router-dom";
+import { useSore } from "../../store/globalState.js";
 const Home = lazy(() => import("../../components/Home.jsx"));
 const Services = lazy(() => import("../../components/Services.jsx"));
 const Offer = lazy(() => import("../../components/Offer.jsx"));
@@ -12,13 +12,17 @@ const ParkingRules = lazy(() => import("../../components/ParkingRules.jsx"));
 const Zoning = lazy(() => import("../../components/Zoning.jsx"));
 
 export default function Router() {
-  const { modalOpen } = functions();
+  const location = useLocation();
+  const { dataMain } = useSore();
+
+  console.log("dataMain", dataMain);
+
   return (
     <>
       <Header />
       <div>
         <Suspense fallback={<p>Loading...</p>}>
-          <ScrollToTop />
+          {location?.hash.length < 1 && <ScrollToTop />}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/services" element={<Services />} />
@@ -29,7 +33,6 @@ export default function Router() {
           </Routes>
         </Suspense>
       </div>
-      {modalOpen && <ModalMenu />}
     </>
   );
 }
